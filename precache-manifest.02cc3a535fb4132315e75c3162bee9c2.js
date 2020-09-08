fee52efb9fba91426cf0dd34c9a4374dd8d2d471
Shiro-Nek0 <tagiki5@gmail.com>
2020-09-08 11:48:55 -0300

    Updates

---
+++ precache-manifest.02cc3a535fb4132315e75c3162bee9c2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e24881db48c780a4477a64c964d3c8",
+    "url": "/cursos/index.html"
+  },
+  {
+    "revision": "004731d696ab7fc9ba5d",
+    "url": "/cursos/static/css/main.703585cd.chunk.css"
+  },
+  {
+    "revision": "e3fb35a763bee26eee0f",
+    "url": "/cursos/static/js/2.be05d3b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursos/static/js/2.be05d3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004731d696ab7fc9ba5d",
+    "url": "/cursos/static/js/main.3ffdfb19.chunk.js"
+  },
+  {
+    "revision": "cca1b6da47431fefc33b",
+    "url": "/cursos/static/js/runtime-main.309d0b3a.js"
+  },
+  {
+    "revision": "2be11b899fbd4f58a60f15e56cc12799",
+    "url": "/cursos/static/media/abrir.2be11b89.svg"
+  },
+  {
+    "revision": "34ad5712f620bf805264ec283e5ff770",
+    "url": "/cursos/static/media/back.34ad5712.svg"
+  },
+  {
+    "revision": "427c086e89bd799154d99b9ec52d5c9f",
+    "url": "/cursos/static/media/give-classes.427c086e.svg"
+  },
+  {
+    "revision": "3046e91c3c6582de1713a4008dafd5d0",
+    "url": "/cursos/static/media/landing.3046e91c.svg"
+  },
+  {
+    "revision": "6bf8c9a22f0cf2ef5d417642caace73b",
+    "url": "/cursos/static/media/study.6bf8c9a2.svg"
+  }
+]);
